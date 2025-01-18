60f4dc74b60f7f3a5b583a23c8304a967da35bc3
giangallino <99291206+gianlucaegallino@users.noreply.github.com>
2025-01-18 10:42:38 -0300

    Add assets

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+import '../styles/App.css'
+import ScoreBoard from './ScoreBoard.jsx'
+import GameBoard from './GameBoard.jsx'
+
+function App() {
+ //state selected difficulty
+
+
+
+ //there should be a scoreboard component that takes in currscore and maxscore
+
+
+
+ //gameboard component. 
+  //effect that gets the amount of images based on diff 
+  //keep the amount of cards
+  //make card objects
+  //implement fisher yates shuffle
+  //hashmap to keep track of visited
+  //add win condition
+
+  return (
+    <>
+    <header><h1 className="title">PokeMemo</h1></header>
+    <body>
+      <ScoreBoard />
+      <GameBoard />
+    </body>
+    </>
+  )
+}
+
+export default App
